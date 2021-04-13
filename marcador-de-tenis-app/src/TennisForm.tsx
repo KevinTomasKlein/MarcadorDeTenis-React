ffe3fed7239cecf0452b30c51e955fe795c87c6e
KevinTomasKlein <kevin.tomas@biko2.com>
2021-04-13 18:20:22 +0200

    Kevin:Formulario inicial para realizar pruebas

--- marcador-de-tenis-app/src/TennisForm.tsx
+++ marcador-de-tenis-app/src/TennisForm.tsx
@@ -2,17 +2,29 @@ import "bootstrap/dist/css/bootstrap.css";
 
 export function TennisForm(): any {
   return (
-    <div className="container row">
-      <div className="col-md-12">
-        <div className="col-md-6">
-          <label htmlFor="player1Name"></label>
-          <input type="text" id="player1Name"></input>
+    <body>
+      <div
+        className="container w-75"
+        style={{ backgroundColor: "lightblue", height: "500px" }}
+      >
+        <h1 className="text-center">TENNIS GAME</h1>
+        <div className="col-md-12 justify-content-around d-flex ">
+          <div className="col-md-3  mb-5 pb-1">
+            <label htmlFor="player1Name">JUGADOR 1</label>
+            <input type="text" id="player1Name"></input>
+          </div>
+          <div className="col-md-3">
+            <p className="text-muted py-5">Soy el texto de la partida</p>
+          </div>
+          <div className="col-lg-3  mb-5">
+            <label htmlFor="player2Name">JUGADOR 2</label>
+            <input type="text" id="player2Name"></input>
+          </div>
         </div>
-        <div className="col-md-6">
-          <label htmlFor="player2Name"></label>
-          <input type="text" id="player2Name"></input>
+        <div className="d-flex justify-content-center pb-5">
+          <input className="button" type="submit" value="Play!"></input>
         </div>
       </div>
-    </div>
+    </body>
   );
 }
